c5d4fa250c26e8a15c389b20d6341563e9663bd4
JustJill813 <justjill813@gmail.com>
2025-07-14 14:04:15 -0400

    changed layout of 3rd page

--- public/js/details.js
+++ public/js/details.js
@@ -3,6 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
   if (!recipe) return;
 
   document.getElementById('recipe-title').textContent = recipe.name;
+  document.getElementById('servings').textContent = recipe.servings || '—';
+  document.getElementById('prepTime').textContent = recipe.prepTime || '—';
+  document.getElementById('cookTime').textContent = recipe.cookTime || '—';
   document.getElementById('recipe-ingredients').textContent = recipe.ingredients;
   document.getElementById('recipe-instructions').textContent = recipe.instructions;
+  document.getElementById('recipe-notes').textContent = recipe.notes || 'None';
 });
